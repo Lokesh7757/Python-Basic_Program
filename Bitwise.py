57d317cf4b04d88af74f9647e8c299e45564964f
Lokesh Khankari <159478330+Lokesh7757@users.noreply.github.com>
2024-07-20 08:32:43 +0530

    Add files via upload

---
+++ Bitwise.py
@@ -0,0 +1,20 @@
+a = 10;
+b = 20;
+
+c = a & b;
+print("value of c:", c);
+
+c = a | b;
+print("value of c:",c)
+
+c = a ^ b;
+print("value of c:",c)
+
+c = ~a;
+print("value of c:",c)
+
+c = a<<2;
+print("value of c",c)
+
+c= a>>2;
+print("value of c:",c)
---
+++ Class.py
@@ -0,0 +1,10 @@
+class student1:
+    def __init__ (self, name, salary):       
+        self.name = name
+        self.salary = salary
+
+    s1=student1
+    ("Lokesh",37000)
+
+    print(s1.name)
+    print (s1.salary)
