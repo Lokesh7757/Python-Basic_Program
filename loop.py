7093cb0bf3762635a112ee4689eaa30dd93d6350
Lokesh Khankari <159478330+Lokesh7757@users.noreply.github.com>
2024-07-24 18:55:43 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,22 @@
+# count = 1
+# while count <= 5:
+    
+#     print("Lokesh")
+#     count = count +1
+
+# i = 100
+# while i > 9:
+#     print(i)
+#     i = i -1
+
+# i = 1
+# while (i<=10):
+#     print(i*300)
+#     i = i + 1
+
+i = 1
+while (i<=5):
+    print(i)
+    if (i==1):
+        break
+    i = i + 1
