60bef9b4faa20cb9b833a1bb489a7566ae4a4338
Lokesh Khankari <159478330+Lokesh7757@users.noreply.github.com>
2024-08-07 21:47:42 +0530

    Add files via upload

---
+++ Nested_if.py
@@ -0,0 +1,16 @@
+a = 25
+p = 15
+x = 20
+
+if a > p: # 10 > 15
+
+    if a > x: # 10 > 20
+        print("A is Highest1")
+    else:
+        print("X is Highest2")
+else:
+    if p > x: # 15 > 20
+        print("P is Highest3")
+    else:
+        print("X is Highest4")
+
