aba0460852048a3dc6bcaf5608cc1d5d6348d0d1
Lokesh Khankari <159478330+Lokesh7757@users.noreply.github.com>
2024-08-15 19:09:01 +0530

    Add files via upload

---
+++ Employee_ClassObject.py
@@ -0,0 +1,11 @@
+class Employee: #class
+    employee_id = 0
+
+employee1 = Employee()  #Object
+employee2 = Employee()
+
+employee1 = employee_id = 150  #access variable
+print(employee_id)
+
+employee2 = employee_id = 420
+print(employee_id)
