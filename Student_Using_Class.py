2ba223f9e501c97f63132165969a07718b053621
Lokesh Khankari <159478330+Lokesh7757@users.noreply.github.com>
2024-08-15 19:10:05 +0530

    Add files via upload

---
+++ Student_Using_Class.py
@@ -0,0 +1,18 @@
+class Student:
+    Name = ""
+    Std = ""
+    Roll_No = 0
+
+s1 = Student()
+s2 = Student()
+
+s1.Name = "Lokesh"
+s1.Std = "Mca"
+s1.Roll_No = 37
+
+s2.Name = "Sunny"
+s2.Std = "SyMca"
+s2.Roll_No = 101
+
+print(f"Name = {s1.Name}, Std = {s1.Std}, Roll No = {s1.Roll_No}")
+print(f"Name = {s2.Name}, Std = {s2.Std}, Roll No = {s2.Roll_No}")
